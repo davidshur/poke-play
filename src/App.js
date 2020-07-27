c1d6b81c57e448461d38684f10eb839f49953ec9
davidshur <shurdave@gmail.com>
2020-07-27 00:35:12 -0700

    add fetch hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
 import BattleBox from './components/BattleBox';
 
 const App = () => {
-  const [firstPokemon, setFirstPokemon] = useState(0);
-  const [secondPokemon, setSecondPokemon] = useState(0);
-
-  const handleClick = () => {
-    setFirstPokemon(Math.floor(Math.random() * 151));
-    setSecondPokemon(Math.floor(Math.random() * 151));
-  };
-
   return (
     <div className="App">
       <div className="Main">
