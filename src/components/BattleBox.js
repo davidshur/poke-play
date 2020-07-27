4229ee752363f2c09cd3047a89a36a2ffb2e491a
davidshur <shurdave@gmail.com>
2020-07-27 00:31:16 -0700

    move content out of appjs

--- src/components/BattleBox.js
+++ src/components/BattleBox.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const BattleBox = () => ();
+const BattleBox = () => {
+  const [firstPokemon, setFirstPokemon] = useState(0);
+  const [secondPokemon, setSecondPokemon] = useState(0);
+
+  const handleClick = () => {
+    setFirstPokemon(Math.floor(Math.random() * 151));
+    setSecondPokemon(Math.floor(Math.random() * 151));
+  };
+
+  return (
+    <>
+      <div className="Splitter">
+        <header className="Battler">
+          <p>Pokemon #{firstPokemon}</p>
+        </header>
+        <div className="Versus">
+          <h4>VS</h4>
+        </div>
+        <header className="Battler">
+          <p>Pokemon #{secondPokemon}</p>
+        </header>
+      </div>
+      <div>
+        <button className="Button" onClick={handleClick}>
+          BATTLE
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default BattleBox;
