b89b562756c99afcd9d3d01ff49d59f7ffcc3222
davidshur <shurdave@gmail.com>
2020-07-27 00:37:41 -0700

    add hooks folder and useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+
+  useEffect(async () => {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    setResponse(json);
+  });
+
+  return response;
+};
+
+export default useFetch;
