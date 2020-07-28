43bbd231abbf9a12b5bc1bd6eac82a8a73e82e75
davidshur <shurdave@gmail.com>
2020-07-27 22:52:12 -0700

    landing page works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
+import Landing from './components/Landing';
 import BattleBox from './components/BattleBox';
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className='Main'>
+    <div className="App">
+      <div className="Main">
         <Header />
-        <BattleBox />
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/battle" component={BattleBox} />
+        </Router>
       </div>
     </div>
   );
