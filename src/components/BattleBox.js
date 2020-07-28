25d54be3595e30d0511bbd67063bce0ce74c95c5
davidshur <shurdave@gmail.com>
2020-07-27 23:31:39 -0700

    add outline on click

--- src/components/BattleBox.js
+++ src/components/BattleBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pokemon from '../components/Pokemon';
 
 import useFetch from '../hooks/useFetch';
@@ -8,6 +8,8 @@ const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/';
 const BattleBox = () => {
   const [firstPokemonData, setFirstPokemonUrl] = useFetch();
   const [secondPokemonData, setSecondPokemonUrl] = useFetch();
+
+  const [selected, setSelected] = useState('left');
 
   const handleClick = () => {
     const firstPokemonId = Math.floor(Math.random() * 151);
@@ -20,14 +22,32 @@ const BattleBox = () => {
   return (
     <>
       <div className="Splitter">
-        <header className="Battler">
-          {firstPokemonData.sprites && <Pokemon data={firstPokemonData} />}
+        <header className="Battler" onClick={() => setSelected('left')}>
+          {firstPokemonData.sprites && (
+            <div
+              className="Selector"
+              style={
+                selected === 'left' ? { width: '100px' } : { border: 'none' }
+              }
+            >
+              <Pokemon data={firstPokemonData} />
+            </div>
+          )}
         </header>
         <div className="Versus">
           <h4>VS</h4>
         </div>
-        <header className="Battler">
-          {secondPokemonData.sprites && <Pokemon data={secondPokemonData} />}
+        <header className="Battler" onClick={() => setSelected('right')}>
+          {secondPokemonData.sprites && (
+            <div
+              className="Selector"
+              style={
+                selected === 'right' ? { width: '100px' } : { border: 'none' }
+              }
+            >
+              <Pokemon data={secondPokemonData} />
+            </div>
+          )}
         </header>
       </div>
       <div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = () => {
-  const [wins, setWins] = useState(0);
-
-  const handleClick = () => {
-    setWins(0);
-  };
-
-  return (
-    <div className="Landing">
-      <h3>Wanna play?</h3>
-      <Link to="/battle" onClick={handleClick}>
-        <button className="Button">BEGIN</button>
-      </Link>
-    </div>
-  );
-};
+const Landing = () => (
+  <div className="Landing">
+    <h3>Wanna play?</h3>
+    <Link to="/battle">
+      <button className="Button">BEGIN</button>
+    </Link>
+  </div>
+);
 
 export default Landing;
